06d7e149d12b23e3c173de21965d424452a7ada2
Sergio Sanchez <ssanchez@alluma.org>
2020-06-17 11:25:12 -0700

    cleaned

---
+++ tacosdedatos_utils/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Una coleccion de herramientas para facilitar el analisis y visualizacion de datos por @tacosdedatos."""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
---
+++ tacosdedatos_utils/__main__.py
@@ -0,0 +1,66 @@
+# type: ignore[attr-defined]
+
+import random
+from enum import Enum
+from typing import Optional
+
+import typer
+from rich.console import Console
+
+from tacosdedatos_utils import __version__
+from tacosdedatos_utils.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="tacosdedatos-utils",
+    help="Una coleccion de herramientas para facilitar el analisis y visualizacion de datos por @tacosdedatos.",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(value: bool):
+    """Prints the version of the package."""
+    if value:
+        console.print(
+            f"[yellow]tacosdedatos-utils[/] version: [bold blue]{__version__}[/]"
+        )
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Name of person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for name. If not specified then choice will be random.",
+    ),
+    version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the tacosdedatos-utils package.",
+    ),
+):
+    """Prints a greeting for a giving name."""
+    if color is None:
+        # If no color specified use random value from `Color` class
+        color = random.choice(list(Color.__members__.values()))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
