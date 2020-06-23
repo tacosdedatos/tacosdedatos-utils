0a2b9cf2d12f607a1beaed686fed6c36a0503681
Sergio Sánchez Zavala <chekos.data@gmail.com>
2020-06-23 06:37:58 -0700

    fix typo

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ html_theme_options = {
     "use_issues_button": True,
     "use_edit_page_button": True,
     "launch_buttons": {
-      "binderhub_url": "https://mybinder.org/v2/gh/tacosdedatos/tacosdedatos-utils/v1.0.0"
+      "binderhub_url": "https://mybinder.org/v2/gh/tacosdedatos/tacosdedatos-utils/v1.0.0",
       "thebelab": True
 }
 
