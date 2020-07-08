0ba1a74c473a520437304416f4b63b403b809da6
Sergio Sanchez <ssanchez@alluma.org>
2020-07-08 13:49:16 -0700

    agrega primera funcion de manejo de datos

---
+++ tacosdedatos_utils/manejo_de_datos.py
@@ -0,0 +1,24 @@
+"""Utilidades para el manejo de datos."""
+
+from IPython.display import display_html
+
+
+def mostrar_dfs(*dfs, espacio: int = 20) -> None:
+    """Muestra más de un DataFrame o Serie de pandas en una celda de jupyter notebooks.
+
+    Parameters
+    ----------
+    espacio : int, optional
+        El espacio entre cada DataFrame en pixeles, por default 20 
+    """
+    html = f"""<div style="display:flex;">"""
+    for df in dfs:
+        try:
+            html_del_df = df._repr_html_()
+        except AttributeError:
+            html_del_df = f"<pre>{repr(df)}</pre>"
+
+        html += f"<div style='margin-right:{espacio}px;'>{html_del_df}</div>"
+
+    html += "</div>"
+    display_html(html, raw=True)
